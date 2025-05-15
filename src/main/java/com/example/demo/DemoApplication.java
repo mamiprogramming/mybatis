5fefe0c8778869ee4ee5fabae7c1e9dfac347420
mamiprogramming <mamiprogramming@gmail.com>
2025-05-16 01:53:57 +0900

    MyBatis実装

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.demo.dao")  // ← Mapperインターフェースのパッケージを指定
+@MapperScan("com.example.demo.dao")  // ← Mapperインターフェースのパッケージを指定 差分
 public class DemoApplication {
 
 	public static void main(String[] args) {
